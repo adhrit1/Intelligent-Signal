900a83c08fc65c6ec30ec9a66e688b6ed3a8d6e6
adhrit1 <adhrit_dev@yahoo.com>
2021-01-23 08:31:25 +0000

    More Updates

--- sensors.py
+++ sensors.py
@@ -1,57 +1,71 @@
-from traffic import red, green
+#The road width is assumed and is taken before the program starts
 
-def sound_sensor(freqeuncy_road1,frequency_road2,volume1,volume2,):
-  ambulance_frequency = 900
+road_width_1=5
+road_width_2=6
 
-  if ambulance_frequency>freqeuncy_road1 and volume1>volume2:
-    green()
-    print("For road 1")
-    red()
-    print("For road 2")
-  elif ambulance_frequency>frequency_road2 and volume2>volume1:
-    red()
-    print("For road 1")
-    green()
-    print("For road 2")
-  elif ambulance_frequency == freqeuncy_road1 and ambulance_frequency==frequency_road2:
-    if volume1>volume1:
-      green()
-      print("For road 1")
-      red()
-      print("For road 2") 
-    elif volume1>volume1:
-      green()
-      print("For road 1")
-      red()
-      print("For road 2")
-
-    
-  
-road_width1=int(input("Enter the width of road 1 in metres:"))
-road_width2=int(input("Enter the width of road 2 in metres:"))
-
-def distance_sensor(Distance1_A, Distance1_B,Distance1_C, Distance1_D, Distance1_E, Distance2_A,Distance2_B,Distance2_C,Distance2_D,Distance2_E):
-  road_traffic_lenght1 = 0
-  road_traffic_lenght1 = 0
-  if Distance1_A < road_width1:
-    road_traffic_lenght1 = 10
-  if Distance1_B < road_width1:
-    road_traffic_lenght1 = 20
-  if Distance1_C < road_width1:
-    road_traffic_lenght1 = 30
-  if Distance1_D < road_width1:
-    road_traffic_lenght = 40
-  if Distance1_E < road_width1:
-    road_traffic_lenght1 = 50
-  if Distance2_A < road_width2:
-    road_traffic_lenght2 = 10
-  if Distance2_B < road_width2:
-    road_traffic_lenght2 = 20
-  if Distance2_C < road_width2:
-    road_traffic_lenght2 = 30
-  if Distance2_D < road_width2:
-    road_traffic_lenght2 = 40
-  if Distance2_E < road_width2:
-    road_traffic_lenght2 = 50
+traffic_1=0
+traffic_2=0
+def dist_sensors(dist1_A, dist1_B, dist1_C, dist1_D, dist1_E,  dist2_A, dist2_B, dist2_C, dist2_D, dist2_E,):
+  road_traffic_length1=0 
+  road_traffic_length2=0 
   
   
+  if dist1_A <= road_width_1: 
+     road_traffic_length1 = 10
+  if dist1_B <= road_width_1: 
+     road_traffic_length1 = 20
+  if dist1_C <= road_width_1: 
+     road_traffic_length1 = 30
+  if dist1_D <= road_width_1: 
+     road_traffic_length1 = 40
+  if dist1_E <= road_width_1: 
+     road_traffic_length1 = 50
+  if dist2_A <= road_width_2: 
+     road_traffic_length2 = 10
+  if dist2_B <= road_width_2: 
+     road_traffic_length2 = 20
+  if dist2_C <= road_width_2: 
+     road_traffic_length2 = 30
+  if dist2_D <= road_width_2: 
+     road_traffic_length2 = 40
+  if dist2_E <= road_width_2: 
+     road_traffic_length2 = 50
+
+  road1 = 0
+  road2 = 1
+
+  if road1==0 and road2==1:
+    algo_num = road_width_2/road_width_1
+    algo_num = algo_num*100
+    road_value_1 = road_traffic_length1*road_width_1
+    #time
+
+  elif road1==1 and road2==0:
+    algo_num = road_width_1/road_width_2
+    algo_num = algo_num*100
+    road_value_2 = road_traffic_length2*algo_num
+
+  return road_traffic_length1,road_traffic_length2
+
+#subjcet to change- for testing
+        
+
+def sound_sensor(ambulance_frequency,volume1,volume2):  
+  sound_road1 = int(input("Frequency reading for road 1: "))
+  sound_road2 = int(input("Frequency reading for road 2: "))
+  
+  if ambulance_frequency == sound_road1 and volume1 > volume2:
+    print("Signal Green For Road 1")
+    print("Signal Red For road 2")
+ 
+  elif ambulance_frequency == sound_road2 and volume2 > volume1:
+    print("Signal Green For Road 2")
+    print("Signal Red For road 1")
+  
+  elif ambulance_frequency == sound_road1 and ambulance_frequency == sound_road2:
+    if volume1 > volume2:
+     print("Signal Green For Road 1")
+     print("Signal Red For road 2")
+    else:
+      print("Signal Green For Road 2")
+      print("Signal Red For Road 1")
--- traffic.py
+++ traffic.py
@@ -1,35 +1,40 @@
-import time
-import random
-from sensors import proximity_sensors
+import time 
+
 
 def green():
-  print("Signal Green" , end="")
+  print("Signal Green ",end="")
 
 def red():
-  print("Signal Red" , end="")
+  print("Signal Red ",end="")
 
 def timer(t):
-  while t:
-    mins, secs = divmod(t,60)
-    timer = "{:02d}:{:02d}".format(mins,secs)
-    print(timer,end="")
-    time.sleep(1)
-    t -= 1
-    if t == 0:
-      print(f'00:00')
+  while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+        if t==0:
+          print(f'00:00')
 
-def signals(traffic_road1,traffic_road2):
-  if traffic_road1 >= traffic_road2:
-    green()
-    print("For Road 1")
-    red()
-    print("For Road 2")
-    traffic_road1 = traffic_road1 -50
-    timer(20)
-  elif traffic_road2 < traffic_road1:
-    green()
-    print("For road 2")
-    red()
-    print("For road 1")
-    traffic_road2 = traffic_road2 - 50
-    timer(20)
+def signals(traffic_road1,traffic_road2,):
+  
+  
+  while (traffic_road1 and traffic_road2) >= 0:
+    #checks the amount of traffic on both roads
+    if traffic_road1 >= traffic_road2:
+      green()
+      print("For Road 1")
+      red()
+      print("For Road 2")
+      #traffic in that time gone by
+      traffic_road1 = traffic_road1 - 40
+      timer(20)
+    elif traffic_road2 > traffic_road1:
+      green()
+      print("For Road 2")
+      red()
+      print("For Road 1")
+      #traffic in that time gone by
+      traffic_road2 = traffic_road2 -40
+      timer(20)
